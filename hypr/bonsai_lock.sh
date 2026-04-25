c39f2dd7f9e63fe8f2cf33d8f88ba218299bacc7
Lanisha <lanishaanusri28@gmail.com>
2026-04-25 20:55:10 +0800

    .

--- hypr/bonsai_lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Force a smaller, specific size so it doesn't clash with the logo
-export COLUMNS=40
-export LINES=15
-
-# Generate, strip colors, and escape for Pango
-# We save it to a temporary file
-cbonsai -p -l -L 20 | sed 's/\x1b\[[0-9;]*m//g' | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g' > /tmp/hyprlock_bonsai.txt
